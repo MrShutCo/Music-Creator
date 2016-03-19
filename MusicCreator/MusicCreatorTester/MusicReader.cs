6abb0726eb82f96cccde872df7603725cfbc6581
MrShutCo <tylersafaric99@gmail.com>
2016-03-19 15:41:06 -0400

    Created basic stuff
    ^^

---
+++ MusicCreator/MusicCreatorTester/MusicReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCreatorTester {
+    public static class MusicReader {
+
+        public static Dictionary<string, int> ReadMusicFile(string fileName) {
+            string line;
+            Dictionary<string, int> Notes = new Dictionary<string, int>();
+            StreamReader file = new StreamReader(fileName + ".txt");
+            while ((line = file.ReadLine()) != null){
+                string[] notesLine = line.Split(' ');
+                foreach (string s in notesLine) {
+                    if (!Notes.Keys.Contains(s)){
+                        Notes.Add(s, 1);
+                    }
+                    else {
+                        Notes[s] += 1;
+                    }
+                }
+            }
+            return Notes;
+        }
+
+        public 
+
+    }
+}
---
+++ MusicCreator/MusicCreatorTester/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCreatorTester {
+    class Program {
+        static void Main(string[] args) {
+
+            Dictionary<string, int> MusicalNotes = MusicReader.ReadMusicFile("MusicFile");
+
+            foreach(KeyValuePair<string, int> kvp in MusicalNotes) {
+                Console.WriteLine("Note {0}: {1}", kvp.Key, kvp.Value);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
